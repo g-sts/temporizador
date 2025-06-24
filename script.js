226f024df22e438a15b2ae089472b6fd86fa660e
git status <215340863+g-sts@users.noreply.github.com>
2025-06-24 19:41:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+let hours = document.querySelector('#hours')
+let minutes = document.querySelector('#minutes')
+let seconds = document.querySelector('#seconds')
+let timer 
+let isRunning = false
+
+hours.value = '00'
+minutes.value = '00'
+seconds.value = '00'
+
+document.querySelector('#play').addEventListener('click', start)
+document.querySelector('#pause').addEventListener('click', pause)
+
+function start(){
+    if (!isRunning) {
+        timer = setInterval(updateTime, 1000)
+        isRunning = true
+    }
+}
+
+function pause() {
+    clearInterval(timer)
+    isRunning = false
+}
+
+function updateTime(){
+    let s = parseInt(seconds.value)
+    let m = parseInt(minutes.value)
+    let h = parseInt(hours.value)
+
+    if(s === 0){
+        if(m === 0){
+            if(h === 0){
+                clearInterval(timer)
+                isRunning = false
+                return
+            } else{
+                h--
+                m = 59
+                s = 59
+            }
+
+        }else{
+            m--
+            s = 59
+        }
+    } else{
+        s--
+    }
+    
+    seconds.value = s.toString().padStart(2, '0')
+    minutes.value = m.toString().padStart(2, '0')
+    hours.value = h.toString().padStart(2, '0')
+}
